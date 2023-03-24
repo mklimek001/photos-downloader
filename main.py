13983ef21041b5261ffe6a25985a9cbcec4bb00e
mklimek <klimekmiko@gmail.com>
2023-03-24 12:50:36 +0100

    Upgraded view

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
+from view import View
+
 if __name__ == '__main__':
-    pass
+    View()
+
+
--- view.py
+++ view.py
@@ -2,60 +2,83 @@ import tkinter as tk
 from simple_downloader import get_images
 
 FONT = ("Lato", 12)
+COLOR = "#8fd9d9"
 
 
-def on_submit():
-    num = 0
-    values = []
+class View:
+    def __init__(self):
 
-    test_size = test_slider.get()/100
-    valid_size = valid_slider.get()/100
+        self.root = tk.Tk()
+        self.root.geometry("500x500")
+        self.root.title("Google images downloader")
 
-    if categories.get() != '':
-        values = categories.get().split(';')
-        values = [v.strip() for v in values]
+        self.title = tk.Label(self.root, text="Downloading parameters", font=("Lato", 18))
+        self.title.pack(padx=10, pady=10)
 
-    if number.get() != '':
-        num = int(number.get())
+        self.categories_label = tk.Label(self.root, text="Categories separated by semicolons", font=FONT)
+        self.categories_label.pack(pady=(15, 0))
+        self.categories = tk.Entry(self.root, font=FONT, width=50)
+        self.categories.pack()
 
-    print("Categories: ", values)
-    print("Number: ", num)
-    print("Test set size: ", test_size)
-    print("Valid set size: ", valid_size)
+        self.number_label = tk.Label(self.root, text="Number of photos in each category", font=FONT)
+        self.number_label.pack(pady=(15, 0))
+        self.number = tk.Entry(self.root, font=("Lato", 15), width=8)
+        self.number.pack()
 
-    get_images(values, num, test_size, valid_size)
+        self.test_label = tk.Label(self.root, text="Test set size (%) ", font=FONT)
+        self.test_label.pack(pady=(25, 0))
+        self.test_slider = tk.Scale(self.root, from_=0, to=50, orient='horizontal', width=20, length=300,
+                                    troughcolor="white", activebackground=COLOR)
+        self.test_slider.set(20)
+        self.test_slider.pack()
 
+        self.valid_label = tk.Label(self.root, text="Validation set size (%) ", font=FONT)
+        self.valid_label.pack(pady=(25, 0))
+        self.valid_slider = tk.Scale(self.root, from_=0, to=50, orient='horizontal', width=20, length=300,
+                                     troughcolor="white", activebackground=COLOR)
+        self.valid_slider.set(10)
+        self.valid_slider.pack()
 
-root = tk.Tk()
-root.geometry("500x500")
-root.title("Google images downloader")
+        self.submit = tk.Button(text='Submit', command=self.on_submit, width=20, height=2, font=("Lato", 9), bg=COLOR)
+        self.submit.pack(pady=(25, 0))
 
-title = tk.Label(root, text="Downloading parameters", font=("Lato", 18))
-title.pack(padx=10, pady=10)
+        self.communicate = tk.StringVar()
+        self.communicate.set("")
+        self.communicate_label = tk.Label(self.root, textvariable=self.communicate, font=("Lato", 9))
+        self.communicate_label.pack(pady=(15, 0))
 
-categories_label = tk.Label(root, text="Categories separated by semicolons", font=FONT)
-categories_label.pack(pady=(15, 0))
-categories = tk.Entry(root, font=FONT, width=50)
-categories.pack()
+        self.root.mainloop()
 
-number_label = tk.Label(root, text="Number of photos in each category", font=FONT)
-number_label.pack(pady=(15, 0))
-number = tk.Entry(root, font=("Lato", 15), width=8)
-number.pack()
+    def on_submit(self):
+        self.communicate.set("")
 
-test_label = tk.Label(root, text="Test set size (%) ", font=FONT)
-test_label.pack(pady=(25, 0))
-test_slider = tk.Scale(root, from_=0, to=50, orient='horizontal', width=20, length=300)
-test_slider.set(20)
-test_slider.pack()
+        test_size = self.test_slider.get() / 100
+        valid_size = self.valid_slider.get() / 100
 
-valid_label = tk.Label(root, text="Validation set size (%) ", font=FONT)
-valid_label.pack(pady=(25, 0))
-valid_slider = tk.Scale(root, from_=0, to=50, orient='horizontal', width=20, length=300)
-valid_slider.set(10)
-valid_slider.pack()
+        if self.categories.get().strip() == '':
+            self.communicate.set("Categories field is empty!")
+            self.categories.delete(0, 'end')
+            return
 
-submit = tk.Button(text='Submit', command=on_submit, width=20, height=2)
-submit.pack(pady=(15, 0))
+        elif ',' in self.categories.get() and ';' not in self.categories.get():
+            self.communicate.set("Categories should be separated by ' ; '!")
+            return
 
-root.mainloop()
+        else:
+            values = self.categories.get().split(';')
+            values = [v.strip() for v in values]
+
+        if self.number.get().isnumeric():
+            num = int(self.number.get())
+        else:
+            self.communicate.set("Something is wrong with number field!")
+            self.number.delete(0, 'end')
+            return
+
+        print("Categories: ", values)
+        print("Number: ", num)
+        print("Test set size: ", test_size)
+        print("Valid set size: ", valid_size)
+
+        get_images(values, num, test_size, valid_size)
+        self.communicate.set("Downloading completed!")
